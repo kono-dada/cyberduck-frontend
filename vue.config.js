5fa57260e08735121b5c2d597560d716e86c580a
Congyuwang <congyuwang@link.cuhk.edu.cn>
2022-10-12 18:38:46 +0800

    allow hosts

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    allowedHosts: [
+        "duck.forkingpark.cn",
+        "clownfish-app-iez76.ondigitalocean.app"
+    ]
+  }
 })
