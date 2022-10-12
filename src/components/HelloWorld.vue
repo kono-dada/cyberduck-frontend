f729653bc0e62502ef32ca8c3f3367124bcdab10
Congyuwang <congyuwang@link.cuhk.edu.cn>
2022-10-12 18:09:58 +0800

    test vue fetching user info

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,54 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h2>Ducks Viewed Are Listed Below</h2>
+    <div v-for="duck in this.userState.duckHistory" v-bind:key="duck.id">
+      <h3>{{duck.title.cn}}</h3>
+      <p>{{duck.story.cn}}</p>
+      <div v-if="duck.relatedExhibit">
+        <h4>相关展品</h4>
+        <div>名称：duck.relatedExhibit.title.cn</div>
+        <div>地点：duck.relatedExhibit.location.cn</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      userState: {
+        duckHistory: []
+      },
+    }
+  },
+  mounted() {
+    try {
+      getUserInfo(renderUserState);
+    } catch (e) {
+      console.log("failed to get user info: " + e);
+    }
+  }
+}
+
+function renderUserState(dat) {
+  this.userState = dat;
+}
+
+async function getUserInfo(renderUserState) {
+  const response = await axios.get("https://sso.forkingpark.cn/api/user-info");
+  if (response.status === 404) {
+    const current_url = window.location.href;
+    window.location.href = "https://sso.forkingpark.cn/login?redirect_url=" + current_url;
+  } else if (response.status === 200) {
+    renderUserState(response.data.json())
   }
 }
 </script>
