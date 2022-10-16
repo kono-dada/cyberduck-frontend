61ed46815a5df5d74131e60ec710f0b04015443c
kono-dada <121090029@link.cuhk.edu.cn>
2022-10-16 23:06:56 +0800

    1

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,5 @@
-const {defineConfig} = require('@vue/cli-service')
-module.exports = defineConfig({
-    transpileDependencies: true,
-    devServer: {
-        allowedHosts: [
-            "duck.forkingpark.cn",
-            "clownfish-app-iez76.ondigitalocean.app"
-        ],
-        client: {
-            webSocketURL: 'auto://0.0.0.0:0/ws'
-        }
-    }
-})
+module.exports = {
+  transpileDependencies: [
+    'vuetify'
+  ]
+}
