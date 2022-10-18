9604cd6590ed1fbe380c4e39142eb226d6672342
Congyuwang <congyuwang@link.cuhk.edu.cn>
2022-10-19 01:25:19 +0800

    allow hosts

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  devServer: {
+    allowedHosts: [
+      "duck.forkingpark.cn",
+      "clownfish-app-iez76.ondigitalocean.app"
+    ],
+    client: {
+      webSocketURL: 'auto://0.0.0.0:0/ws'
+    }
+  }
 }
