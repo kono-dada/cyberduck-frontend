40a4613d84f5623e8448b220ba48e1bcd0e15231
Congyuwang <congyuwang@link.cuhk.edu.cn>
2022-10-20 10:55:30 +0800

    do not set starting zoom level too small

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -223,7 +223,7 @@ export default {
       panOnlyWhenZoomed: false,
       minScale: 0.5,
       maxScale: 1.0,
-      startScale: defaultStart.scale,
+      startScale: defaultStart.scale < 0.8 ? 0.8 : defaultStart.scale,
       startX: defaultStart.x,
       startY: defaultStart.y,
       contain: 'outside'
