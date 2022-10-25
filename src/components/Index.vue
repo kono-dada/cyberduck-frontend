2805d5ffc67363a6b5164d832cd5e7835c135e13
Congyuwang <congyuwang@link.cuhk.edu.cn>
2022-10-26 03:12:26 +0800

    Revert "improve ranking card"
    This reverts commit fda3657870f62bdcb0590ffd0209bf13b4e1a14f.

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -78,18 +78,19 @@
       <v-card
           style="text-align: center"
           class="mx-2 nes-container is-rounded"
-          width="95%"
+          width="90%"
       >
         <img class="switches" alt="close" @click="showRanking=false;showDuckList=true;" :src="closeIcon()"
              style="position: absolute; right: 5%; top: 3%; z-index: 5;">
-        <img
-            src="https://parklife-1303545624.cos.ap-guangzhou.myqcloud.com/ducking.png"
-            style="position: absolute; left: 50%; transform: translateX(-50%); top: 25px; image-rendering: pixelated"
-            alt="duck-king"
-        >
-        <v-card-title style="alignment: center; line-height: 2rem; padding-top: 25px">
-          <h1>{{ language === 'cn' ? '鸭王' : 'King of Ducks' }}</h1>
+        <v-card-title style="alignment: center">
+          <img
+              src="https://parklife-1303545624.cos.ap-guangzhou.myqcloud.com/ducking.png"
+              alt="duck-king"
+          >
         </v-card-title>
+        <v-card-subtitle style="padding-top: 25px; line-height: 2rem">
+          <h1>{{ language === 'cn' ? '鸭王' : 'King of Ducks' }}</h1>
+        </v-card-subtitle>
         <v-card-text>
           <p style="font-size: larger">
             {{ language === 'cn' ? '恭喜你' : 'Congratulations!' }}
